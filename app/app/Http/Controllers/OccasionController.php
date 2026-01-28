5c394f23a0dbf383493faadd9c99dbada0e2e7cd
Aki <103581174+Akii-Dev@users.noreply.github.com>
2026-01-28 14:36:36 +0100

    add occasion storing functionality, add validation for occasion input, error and succes logging

--- app/app/Http/Controllers/OccasionController.php
+++ app/app/Http/Controllers/OccasionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Occasion;
 use Illuminate\Http\Request;
 
 class OccasionController extends Controller
@@ -15,5 +16,33 @@ class OccasionController extends Controller
         
 
         return view('occasion.create');
+    }
+
+    public function store(Request $request)
+    {
+        // check if user is authenticated
+        if (!session()->has('user')) {
+            return redirect('/login');
+        }
+
+        $validated = $request->validate([
+            'title' => 'required|string|unique:occasions,title',
+            'price' => 'required|numeric|max:20000', // cars cannot cost more than 20k
+            'plate' => 'required|string',
+            'mileage' => 'required|numeric|max:2147483647', // literally just the integer limit
+            'description' => 'required|string|max:500',
+        ]);
+
+        // there isn't really a difference between calling the factory or not
+        $booking = Occasion::factory()->create([
+            'title' => $validated['title'],
+            'price' => $validated['price'],
+            'plate' => $validated['plate'],
+            'mileage' => $validated['mileage'],
+            'description' => $validated['description'],
+        ]);
+
+        return redirect()->route('admin.index')->with('success', 'Occasion aangemaakt!');
+
     }
 }
--- app/resources/views/occasion/create.blade.php
+++ app/resources/views/occasion/create.blade.php
@@ -10,10 +10,15 @@
 </head>
 
 <body class="bg-yellow-50">
-    <form>
+
+    
+    <form action="{{ route('occasion.store') }}" method="POST">
         @csrf
 
         <div class="flex flex-col py-4 px-8 m-16 border border-yellow-300 rounded-lg bg-white shadow-sm">
+
+            {{-- start 1/3 --}}
+
             <div class="mb-4">
                 <label for="title" class="block text-sm/6 font-medium text-gray-900">Titel*</label>
                 <div class="mt-2">
@@ -46,17 +51,38 @@
                 </div>
             </div>
 
-
+            {{-- end 1/3 --}}
 
             <div>
                 <label for="description" class="block text-sm/6 font-medium text-gray-900">Beschrijving*</label>
                 <div class="mt-2">
                     <textarea id="description" name="description" rows="4"
-                        class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+                        class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-yellow-600 sm:text-sm/6"></textarea>
                 </div>
             </div>
-
             
+            {{-- errors declared in session --}}
+    @if (session('error'))
+        <div class="bg-red-500">
+            {{ session('error') }}
+        </div>
+    @endif
+    {{-- input validation errors --}}
+    @if ($errors->any())
+        <div>
+            @foreach ($errors->all() as $error)
+                <p class="bg-red-500">{{ $error }}</p>
+            @endforeach
+        </div>
+    @endif
+
+            <div class="mt-6 flex items-center justify-end gap-x-6">
+                <a href="{{ route('admin.index') }}" class="text-sm/6 font-semibold text-gray-900">Annuleer en ga terug</a>
+                <button type="submit"
+                    class="rounded-md bg-yellow-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-yellow-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600">Save</button>
+            </div>
+
+
 
 
 
